06c944279d62f769189f0eaf1b1906d327b861c8
寒露 <takats.cnt@gmail.com>
2024-01-18 11:15:46 +0900

    Add biome
    ```
    npm install --save-dev --save-exact @biomejs/biome
    npm install --save-dev npm-run-all
    npm uninstall --save-dev eslint eslint-plugin-react-hooks eslint-plugin-react-refresh @typescript-eslint/esling-plugin @typescript-eslint/parser
    ```

--- genshin-score/src/main.tsx
+++ genshin-score/src/main.tsx
@@ -1,10 +1,11 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import '@assets/index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.tsx";
+import "@assets/index.css";
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
+const rootElement = document.getElementById("root") as HTMLElement;
+ReactDOM.createRoot(rootElement).render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
-)
+);
