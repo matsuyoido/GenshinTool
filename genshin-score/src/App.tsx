eb3132f0d2e22b57df04bcd2280fad07b41da046
寒露 <takats.cnt@gmail.com>
2024-01-21 16:15:51 +0900

    Add react-router & chakraUI

--- genshin-score/src/App.tsx
+++ genshin-score/src/App.tsx
@@ -1,33 +1,24 @@
-import { useState } from "react";
-import reactLogo from "@assets/react.svg";
-import viteLogo from "/vite.svg";
-import "@assets/App.css";
+import { Routes, Route, Outlet } from "react-router-dom";
+import Example from "@page/example";
 
-function App() {
-  const [count, setCount] = useState(0);
+const Layout = (): JSX.Element => (
+  <>
+    <h1>HogeHoge</h1>
+    <Outlet />
+  </>
+);
 
+const Home = (): JSX.Element => <>Home!!</>;
+
+function App(): JSX.Element {
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank" rel="noreferrer">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank" rel="noreferrer">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button type="button" onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route path="" element={<Home />} />
+          <Route path="example" element={<Example />} />
+        </Route>
+      </Routes>
     </>
   );
 }
--- genshin-score/src/main.tsx
+++ genshin-score/src/main.tsx
@@ -1,11 +1,31 @@
 import React from "react";
+import { BrowserRouter as Router } from "react-router-dom";
 import ReactDOM from "react-dom/client";
+import {
+  ChakraProvider,
+  extendTheme,
+  withDefaultColorScheme,
+  theme as baseTheme,
+} from "@chakra-ui/react";
 import App from "./App.tsx";
-import "@assets/index.css";
 
+// https://chakra-ui.com/docs/styled-system/customize-theme
+const theme = extendTheme(
+  {
+    colors: {
+      // https://chakra-ui.com/docs/styled-system/theme
+      brand: baseTheme.colors.teal,
+    },
+  },
+  withDefaultColorScheme({ colorScheme: "brand" }),
+);
 const rootElement = document.getElementById("root") as HTMLElement;
 ReactDOM.createRoot(rootElement).render(
   <React.StrictMode>
-    <App />
+    <Router>
+      <ChakraProvider theme={theme}>
+        <App />
+      </ChakraProvider>
+    </Router>
   </React.StrictMode>,
 );
